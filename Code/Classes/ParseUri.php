8f0fa9cdb4a4f7738c639397c82d7ed96f7ebadb
sfritts <stewart.fritts@gmail.com>
2021-08-10 10:25:44 -0500

    Parse URI supporting files.

---
+++ Code/Classes/ParseUri.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Classes;
+
+/**
+ *
+ * @author Stewart Fritts <sfritts@spectrumvoip.com>
+ */
+class ParseUri {
+    //put your code here
+    
+    public $uri;
+    public $defaultController = "main";
+}
---
+++ Code/Controllers/ParentController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Controllers;
+/**
+ *
+ * @author Stewart Fritts <sfritts@spectrumvoip.com>
+ */
+abstract class ParentController {
+    
+}
